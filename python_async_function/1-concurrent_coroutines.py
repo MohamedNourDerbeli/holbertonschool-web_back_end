f5b2784393b6269a6fc9b3b680fbf605221b4dbb
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-04-24 23:26:00 +0100

    add type-annotated

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,11 +3,12 @@
  function that returns the list of all the task
 """
 import asyncio
+from typing import List
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     return the list of all the task
     """
