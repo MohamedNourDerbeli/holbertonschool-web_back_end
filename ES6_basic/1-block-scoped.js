2082c9538176c0def38025b033d764216f13ee25
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-06 09:14:14 +0100

    Block Scope

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  var tasks = false;
+  var tasks2 = true;
 
   if (trueOrFalse) {
-    var test = true;
-    var test = false;
+    var task = true;
+    var task2 = false;
   }
 
-  return [task, task2];
+  return [tasks, tasks2];
 }
