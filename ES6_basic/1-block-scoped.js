c6eb9e1d3b1ed00f2abbe1e2ce775791a2577f02
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-07 18:43:16 +0100

    fix eslint

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  var tasks = false;
-  var tasks2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    const task = true;
+    const task2 = false;
   }
 
-  return [tasks, tasks2];
+  return [task, task2];
 }
