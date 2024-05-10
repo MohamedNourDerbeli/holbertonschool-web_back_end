ecd53b44d7ab58d067e0e9909d45b10a6aad24d9
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-10 09:00:54 +0100

    Vroom

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,3 +1,7 @@
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+
 export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
@@ -6,6 +10,6 @@ export default class Car {
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    return new Car(this[_brand], this[_motor], this[_color]);
   }
 }
