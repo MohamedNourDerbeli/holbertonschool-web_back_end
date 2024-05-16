759e0f100993fa0472af6e757e9928e9d7b67165
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-16 10:10:54 +0100

    task 3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+const { uploadPhoto, createUser } = require('./utils');
+export default function handleProfileSignup() {
+  Promise.all([
+    uploadPhoto().then(photo => photo.body),
+    createUser().then(user => user)
+  ])
+    .then(([photo, user]) => {
+      console.log(photo, user.firstName, user.lastName);
+    })
+    .catch(err => console.log("Signup system offline"));
+
+}
