7887704f834f7d1a8fc467037b283ac28d7b6289
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-16 13:15:39 +0100

    tesk

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,21 @@
 import signUpUser from './4-user-promise'
 import uploadPhoto from './5-photo-reject'
-export default async function handleProfileSignup (firstName, lastName, fileName) {
-	try {
-		const user = await signUpUser(firstName, lastName)
-		const photo = await uploadPhoto(user, fileName)
-		return { user, photo }
-	} catch (error) {
-
-	}
-
+export default async function handleProfileSignup (
+  firstName,
+  lastName,
+  fileName
+) {
+  try {
+    const user = await signUpUser(firstName, lastName)
+    await uploadPhoto(user, fileName)
+		return {
+			status: 'Done',
+			value: user
+		};
+  } catch (error) {
+		return {
+			status: 'rejected',
+			value: error.toString()
+		};
+  }
 }
