51ed5dcc3cbaff616ec2e2c2c4a17c6075b33614
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-16 11:10:44 +0100

    fix erorr

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,9 @@
-import { createUser, uploadPhoto } from './utils';
+const { uploadPhoto, createUser } = require('./utils');
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((data) => {
-      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    .then((ressult) => {
+      console.log(`${ressult[0].body} ${ressult[1].firstName} ${ressult[1].lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => console.log("Signup system offline"));
 }
-
-export default handleProfileSignup;
