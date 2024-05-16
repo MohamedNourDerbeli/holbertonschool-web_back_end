b0d49089fb7eceb471fa46fe964672b0b4d279b6
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-16 10:29:39 +0100

    task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,12 @@
 const { uploadPhoto, createUser } = require('./utils');
-export default function handleProfileSignup() {
-  Promise.all([
-    uploadPhoto().then(photo => photo.body),
-    createUser().then(user => user)
-  ])
-    .then(([photoBody, userBody]) => {
-      console.log(photoBody, userBody.firstName, userBody.lastName);
-    })
-    .catch(error => { console.log("Signup system offline") });
+export default async function handleProfileSignup() {
+  try {
+    const [photoResult, userResult] = await Promise.all([
+      uploadPhoto(),
+      createUser()
+    ]);
+    console.log(photoResult.body, userResult.firstName, userResult.lastName);
+  } catch (error) {
+    console.log("Signup system offline");
+  }
 }
