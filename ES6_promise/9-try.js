dd4dc3c58c9e6082b42e183977a7885d3b5571f2
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-16 22:06:26 +0100

    task

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,8 +1,7 @@
 export default function guardrail(mathFunction) {
 	const queue = [];
 	try {
-		const result = mathFunction();
-		queue.push(result, 'Guardrail was processed');
+		queue.push(mathFunction(), 'Guardrail was processed');
 	} catch (error) {
 		queue.push(error.toString(), 'Guardrail was processed');
 	}
